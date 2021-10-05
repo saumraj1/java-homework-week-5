13fa3a4addd88e1c8dd67bf6ee0314bda137d8b9
saumraj1 <saumraj1@gmail.com>
2021-10-05 13:57:54 +0100

    Add biodata

---
+++ src/BioData.java
@@ -0,0 +1,14 @@
+public class BioData {
+    /* Write a program that prints your BIO Data.*/
+
+     public static void main(String[] args) {
+        System.out.println("Name: Laxmi prajapati");
+        System.out.println("Father's Name: Prahaladbhai");
+        System.out.println("Mother's Name: Manguben");
+        System.out.println("Date of birth: 05/08/1984");
+        System.out.println("Blood Group: B");
+        System.out.println("Mobile number: Mobile Number: 07456733456");
+        System.out.println("Nationality: British");
+        System.out.println("Country: United Kingdom");
+    }
+}
